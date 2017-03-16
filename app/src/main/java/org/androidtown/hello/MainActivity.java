754b7441789214a1bbdb9cf19f36dacd094c12f3
KimKyuBeom <kgb6346@naver.com>
2017-03-16 16:29:07 +0900

    hello world

--- app/src/main/java/org/androidtown/hello/MainActivity.java
+++ app/src/main/java/org/androidtown/hello/MainActivity.java
@@ -1,7 +1,10 @@
 package org.androidtown.hello;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+    public void onButton1Clicked(View v){
+        Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://m.naver.com"));
+        startActivity(myIntent);
+    }
+
+    public void onButton2Clicked(View v){
+        Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri. parse("tel:010-7286-9967"));
+        startActivity(myIntent);
     }
 }
